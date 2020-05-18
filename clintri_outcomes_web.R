5ca37432c2dd6243c1e9b7aa66a339b75f86920e
TaiohY <taioh.yokoyama@etu.unistra.fr>
2020-05-18 15:57:45 +0200

    Minor corrections

--- clintri_outcomes_web.R
+++ clintri_outcomes_web.R
@@ -5,7 +5,6 @@
 #' Returns informations about clinical trials outcomes from CT.gov data base
 #' @param expr Search Expression (sep with +, use AND, OR, ...)
 #' @param fields Study Fields (see https://clinicaltrials.gov/api/info/study_fields_list for a list of available fields)
-#' @param locPath Directory of "AllPublicXML" folder
 #' @param max_rnk Maximum Rank (default is min(numberReturns, 20))
 #' @example clintri_outcomes_web(expr = "COVID+AND+hydroxychloroquine", max_rnk = 20)
 
