c06c0e3b3ea82ecba2c4b98f3373e8ef9739d54c
TaiohY <taioh.yokoyama@etu.unistra.fr>
2020-06-23 14:51:25 +0200

    tsne scatterpie added

--- LDAwithTidytext.R
+++ LDAwithTidytext.R
@@ -1,5 +1,7 @@
 #Latent Dirichlet Allocation (LDA) with tidytext
 
+install.packages("scatterpie")
+
 CT_des1 <- clintri_descriptions_web("COVID", max_rnk = 40)
 CT_des2 <- clintri_descriptions_web("Acute+lymphoblastic+leukemia", max_rnk = 40)
 CT_des3 <- clintri_descriptions_web("Parkinson", max_rnk = 40)
@@ -8,15 +10,21 @@ CT_des1$Grp <- "COVID"
 CT_des2$Grp <- "LAL" 
 CT_des3$Grp <- "Parkinson"
 CT_des4$Grp <- "Flu"
+
 library("dplyr")
+library("tidytext")
+library("tidyr")
+library("topicmodels")
+library("ggplot2")
+library("purrr")
+
 #BDD <- bind_rows(CT_des1, CT_des2)
 BDD <- bind_rows(CT_des1, CT_des2, CT_des3, CT_des4) 
 BDD <- BDD %>% 
   group_by(NCTid) %>% 
   mutate(text = paste0(textblock, collapse = " "))
 BDD <- distinct(BDD[,-1])
-library("tidytext")
-library("tidyr")
+
 data("stop_words")
 BDD$text <- trimws(gsub("\\w*[0-9]+\\w*\\s*", "", BDD$text))
 head(BDD$text)
@@ -29,18 +37,14 @@ BDD_frq <- BDD_td %>%
   count(NCTid, word)%>%
   group_by(NCTid)
 
-library(topicmodels)
-
 BDD_dtm <- BDD_frq %>%
   cast_dtm(NCTid, word, n)
 
 BDD_lda <- LDA(BDD_dtm, k = 4)
-BDD_lda
+
 
 BDD_topics <- tidy(BDD_lda, matrix = "beta")
 BDD_topics
-
-library("ggplot2")
 
 BDD_top_terms <- BDD_topics %>%
   group_by(topic) %>%
@@ -48,7 +52,7 @@ BDD_top_terms <- BDD_topics %>%
   ungroup() %>%
   arrange(topic,-beta)
 
-BDD_top_terms %>%
+Top_term_plot <- BDD_top_terms %>%
   mutate(term = reorder_within(term, beta, topic))%>%
   ggplot(aes(term, beta, fill = factor(topic))) + 
   geom_col(show.legend = F) +
@@ -56,6 +60,39 @@ BDD_top_terms %>%
   coord_flip()+
   scale_x_reordered()
 
-#library("Rtsne")
-#tsne_out1 <- Rtsne(BDD_topics, pca=F, perplexity = 30,theta = 0.0)
-#plot(tsne_out1$Y,col=as.numeric(as.factor(BDD$Grp)), asp=1)
+BDD_documents <- tidy(BDD_lda, matrix = "gamma")
+BDD_documents
+
+
+library("Rtsne")
+tsne_out1 <- Rtsne(gamma_spread, pca=F, perplexity = 50,theta = 0.0)
+tsne_out1
+plot(tsne_out1$Y, asp=1)
+#tsne avec couleurs selon topic détecté pour chaque étude
+plot(tsne_out1$Y,col=as.numeric(as.factor(BDD$Grp)), asp=1)
+
+beta_spread <- BDD_topics %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, beta) %>%
+  filter(topic1 > .001 | topic2 > .001 | topic3 > .001 | topic4 > .001)
+
+gamma_spread <- BDD_documents %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, gamma) %>%
+  filter(topic1 > .001 | topic2 > .001 | topic3 > .001 | topic4 > .001)
+
+gamma_spread
+
+library("tidyr")
+library("scatterpie")
+
+gamma_spread$x <- tsne_out1$Y[,1]
+gamma_spread$y <- tsne_out1$Y[,2]
+BDD_scatterpie <- ggplot() + geom_scatterpie(aes(x=x, y = y, group = document),data = gamma_spread,
+                  cols = c("topic1", "topic2", "topic3", "topic4"), color=NA) + coord_equal()
+
+BDD_scatterpie
+
+length(tsne_out1$Y[,1])
+gamma_spread
+length(tsne_out1$Y[,2])
