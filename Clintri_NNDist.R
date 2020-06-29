d46b1fdd1228c33042210d4786b76ffb0478d78b
TaiohY <taioh.yokoyama@etu.unistra.fr>
2020-06-29 16:07:49 +0200

    Little optimizations

--- Clintri_NNDist.R
+++ Clintri_NNDist.R
@@ -1,10 +1,18 @@
 #Clintri_NNDist
 
+library("dplyr")
+library("tidytext")
+library("tidyr")
+library("topicmodels")
+library("ggplot2")
+library("purrr")
+library("Rtsne")
+
 #Préparation des données avec LDA et t-sne
-CT_des1 <- clintri_descriptions_web("COVID", max_rnk = 40)
-CT_des2 <- clintri_descriptions_web("Acute+lymphoblastic+leukemia", max_rnk = 40)
-CT_des3 <- clintri_descriptions_web("Parkinson", max_rnk = 40)
-CT_des4 <- clintri_descriptions_web("Flu", max_rnk = 40)
+CT_des1 <- clintri_descriptions_web("COVID", max_rnk = 50)
+CT_des2 <- clintri_descriptions_web("Acute+lymphoblastic+leukemia", max_rnk = 50)
+CT_des3 <- clintri_descriptions_web("Parkinson", max_rnk = 50)
+CT_des4 <- clintri_descriptions_web("Flu", max_rnk = 50)
 
 BDD <- bind_rows(CT_des1, CT_des2, CT_des3, CT_des4) 
 BDD <- BDD %>% 
@@ -12,7 +20,7 @@ BDD <- BDD %>%
   mutate(text = paste0(textblock, collapse = " "))
 BDD <- distinct(BDD[,-1])
 BDD$text <- trimws(gsub("\\w*[0-9]+\\w*\\s*", "", BDD$text))
-
+data("stop_words")
 BDD_td <- BDD %>%
   unnest_tokens(word, text, token = "words")%>%
   anti_join(stop_words)
@@ -31,13 +39,24 @@ gamma_spread <- BDD_documents %>%
   spread(topic, gamma) %>%
   filter(topic1 > .001 | topic2 > .001 | topic3 > .001 | topic4 > .001)
 
-tsne_out1 <- Rtsne(gamma_spread, pca=F, perplexity = 50,theta = 0.0)
+tsne_out1 <- Rtsne(gamma_spread, pca=F, perplexity = 10,theta = 0.0)
 
 gamma_spread$x <- tsne_out1$Y[,1]
 gamma_spread$y <- tsne_out1$Y[,2]
-
+# data <- NULL
 data <- gamma_spread[c(1, 6, 7)]
 data
+
+#scatterpie
+BDD_scatterpie <- ggplot() + 
+  geom_scatterpie(aes(x=x, y = y, group = document),data = gamma_spread,
+                  cols = c("topic1", "topic2", "topic3", "topic4"),
+                  color=NA,
+                  alpha = 0.3) +
+  coord_equal()
+
+BDD_scatterpie
+
 #install.packages("spatstat")
 library("spatstat")
 
@@ -49,17 +68,10 @@ dist <- nndist(data[c(2,3)], k=5)
 
 # Test avec le document situé sur la 7e ligne
 data[7,]
-# A tibble: 1 x 3
-#document        x     y
-#<chr>       <dbl> <dbl>
-#  1 NCT00490633  2.95  2.46
-
 dist[7]
-#[1] 0.0001062065
-
 #Boucle pour comparer longueurs des vecteurs
 Neighbor <- NULL
-for(i in 1:160){
+for(i in 1:nrow(data)){
   xN <- data[i,]$x
   yN <- data[i,]$y
   xA <- data[7,]$x
@@ -71,15 +83,10 @@ for(i in 1:160){
   }
   
 }
-Neighbor$NCTid <- Neighbor$NCTid[!is.na(Neighbor$NCTid)]
-Neighbor
-#$NCTid
-#[1] "NCT00490633" "NCT01403649" "NCT01532986" "NCT03830190" "NCT03888287" "NCT04403672"
 
-# Titre des études qui en sortent : 
-#Intervention Study of Face Mask and Hand Sanitizer to Reduce Influenza Transmission (M-FLU)
-#Collaborative Efforts to Increase Flu Vaccination (CollabFlu)
-#Randomized Trial to Test the "Coordinated Care for Health Promotion and Activities in Parkinson's Disease" Intervention in the VA
-#Cost-effectiveness of Nursing Interventions for Patients With PD
-#Parkinson's Disease Inpatient Clinical Knowledge and Management
-#Performance Evaluation of RealDetect™ COVID-19 RT-PCR Kit for the Detection of SARS-CoV-2 Virus
+Neighbor$NCTid <- Neighbor$NCTid[!is.na(Neighbor$NCTid)]
+
+NCTid.list <- as.data.frame(Neighbor)
+
+List <- NCTidToTitles_web(NCTid.list)
+View(List)
